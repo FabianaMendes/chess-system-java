cff7b821cbba34c5b2a71346c673692d9ee04038
Fabiana Mendes <fabianamendesmg@yahoo.com.br>
2020-05-06 18:56:52 -0300

    Queen (and Pawn bugfix)

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -12,6 +12,11 @@ public class Pawn extends ChessPiece {
 	}
 
 	@Override
+	public String toString() {
+		return "P";
+	}
+
+	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
@@ -42,7 +47,7 @@ public class Pawn extends ChessPiece {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() + 2, position.getColumn());
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
@@ -58,9 +63,5 @@ public class Pawn extends ChessPiece {
 		return mat;
 	}
 	
-	@Override
-	public String toString() {
-		return "P";
-	}
 
 }
